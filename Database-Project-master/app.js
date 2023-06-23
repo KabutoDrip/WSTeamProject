b4792b05f4c0683c6e671071a868fa47a17a9cef
BuildTools <justin.d.bench@gmail.com>
2023-06-22 20:12:04 -0600

    fix build command

--- Database-Project-master/app.js
+++ Database-Project-master/app.js
@@ -1,60 +1,36 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const mongodb = require('./db/connect');
-const sample1Routes = require('./routes/sample1');
-const sample2Routes = require('./routes/sample2');
-const axios = require('axios');
-const session = require('express-session');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("./db/connect");
+const { auth } = require("express-openid-connect");
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.OKTA_SECRET,
+  baseURL: "https://kabutodrip.onrender.com",
+  clientID: "okqztuEns5khHgcTwRiOk6qOqt2dQzBc",
+  issuerBaseURL: "https://kubutodrip.us.auth0.com",
+};
+
 
 const port = process.env.PORT || 3001;
 const app = express();
 
-// app.use(session({
-//   secret:process.env.GITHUB_CLIENT_SECRET, resave:false, saveUninitialized:true
-// }))
-// app.get('/login', (req, res) => {
-//   res.redirect(
-//     `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&prompt=consent`,
-//   );
-// });
-
-// app.get('/callback', (req, res) => {
-//   const {code} = req.query
-//   const body = {
-//     client_id: process.env.GITHUB_CLIENT_ID,
-//     client_secret: process.env.GITHUB_CLIENT_SECRET,
-//     code,
-//   };
-//   const opts = { headers: { accept: 'application/json' } };
-//   axios
-//     .post('https://github.com/login/oauth/access_token', body, opts)
-//     .then((_res) => {
-//       req.session.token = _res.data.access_token
-//       res.redirect('/api-docs')
-//     })
-//     .catch((err) => res.status(500).json({ err: err.message }));
-// });
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
     next();
   })
-  .use('/', require('./routes'));
+  .use(auth(config))
+  .use("/", require("./routes"));
 
-// app.get('/logout', (req, res) => {
-//   req.session.token = null
-//     res.redirect(
-//       `/api-docs`,
-//     );
-//   });
-mongodb.initDb((err, mongodb) => {
+mongodb.initDb((err) => {
   if (err) {
     console.log(err);
   } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
-    //console.log(`Connected to DB and listening on ${port}`);
+    app.listen(port, () => {
+      console.log(`Connected to DB and listening on ${port}`);
+    });
   }
 });
