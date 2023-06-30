554e662cb70837db8f5b2ce35150d3b862b444c4
BuildTools <justin.d.bench@gmail.com>
2023-06-30 11:36:13 -0600

    these don't need require auth

--- Database-Project-master/routes/snack.js
+++ Database-Project-master/routes/snack.js
@@ -4,9 +4,9 @@ const snackController = require("../controllers/snack.js");
 const { requiresAuth } = require("express-openid-connect");
 const router = express.Router();
 
-router.get("/", requiresAuth(), (req, res) => res.json("got all"));
-router.get("/:id", requiresAuth(), (req, res) => res.json("got snack"));
-router.get("/random", requiresAuth(), (req, res) => res.json("got random"));
+router.get("/", (req, res) => res.json("got all"));
+router.get("/:id", (req, res) => res.json("got snack"));
+router.get("/random", (req, res) => res.json("got random"));
 // This post is working in the api-docs by creating it.
 router.post("/", requiresAuth(), snackController.createSnack);
 router.put("/", requiresAuth(), (req, res) => res.json("edited that"));
