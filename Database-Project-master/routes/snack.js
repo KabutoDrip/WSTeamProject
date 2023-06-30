90d7173d39b9811d09aef1d93af3cf87510e7391
BuildTools <justin.d.bench@gmail.com>
2023-06-29 20:06:40 -0600

    prettier and remove requires auth from get routes

--- Database-Project-master/routes/snack.js
+++ Database-Project-master/routes/snack.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const snackController = require("../controllers/snack.js");
-const {requiresAuth} = require("express-openid-connect");
+const { requiresAuth } = require("express-openid-connect");
 const router = express.Router();
 
-router.get("/", requiresAuth(), (req, res) => res.json("got all"));
-router.get("/:id", requiresAuth(), (req, res) => res.json("got snack"));
-router.get("/random", requiresAuth(), (req, res) => res.json("got random"));
+router.get("/", (req, res) => res.json("got all"));
+router.get("/:id", (req, res) => res.json("got snack"));
+router.get("/random", (req, res) => res.json("got random"));
 router.post("/", requiresAuth(), snackController.createSnack);
 router.put("/", requiresAuth(), (req, res) => res.json("edited that"));
 router.delete("/", requiresAuth(), snackController.deleteSnack);
