a719650997598e53036a3d02e6bf2634f1900de9
Ecob1 <cob19007@byui.edu>
2023-07-01 09:46:25 -0700

    d

--- Database-Project-master/routes/snack.js
+++ Database-Project-master/routes/snack.js
@@ -1,7 +1,7 @@
 const express = require("express");
 // Calling from the controllers folder the methods post and delete
 const snackController = require("../controllers/snack.js");
-const { requiresAuth } = require("express-openid-connect");
+const {requiresAuth} = require("express-openid-connect");
 const router = express.Router();
 
 router.get("/", (req, res) => res.json("got all"));
@@ -9,6 +9,9 @@ router.get("/:id", (req, res) => res.json("got snack"));
 router.get("/random", (req, res) => res.json("got random"));
 // This post is working in the api-docs by creating it.
 router.post("/", requiresAuth(), snackController.createSnack);
+router.post("/", requiresAuth(), snackController.createCandyPastries);
+router.post("/", requiresAuth(), snackController.createMeats);
+router.post("/", requiresAuth(), snackController.createNutsDried_Fruits);
 router.put("/", requiresAuth(), (req, res) => res.json("edited that"));
 router.delete("/", requiresAuth(), snackController.deleteSnack);
 
