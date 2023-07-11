aff26fc6735255729eb1f99b1fa188f61485dd72
Ecob1 <cob19007@byui.edu>
2023-07-11 11:31:19 -0700

    Add more test

--- Database-Project-master/routes/snack.js
+++ Database-Project-master/routes/snack.js
@@ -7,12 +7,12 @@ const {
   editSnack,
   deleteSnack,
 } = require("../controllers/snack.js");
-const { requiresAuth } = require("express-openid-connect");
+const {requiresAuth} = require("express-openid-connect");
 const router = express.Router();
 
 router.get("/", getAllSnacks);
 router.get("/:id", getSnacksId);
-router.post("/", requiresAuth(), createSnack);
+router.post("/", createSnack);
 
 router.put("/:id", requiresAuth(), editSnack);
 router.delete("/:id", requiresAuth(), deleteSnack);
--- Database-Project-master/test/snack.spec.js
+++ Database-Project-master/test/snack.spec.js
@@ -1,19 +1,74 @@
-// const app = require("../app");
-// const supertest = require("supertest");
-// const {expect} = require("@jest/globals");
-// const request = supertest(app);
+const app = require("../app");
+const supertest = require("supertest");
+const {expect} = require("@jest/globals");
+const request = supertest(app);
+const {MongoClient} = require("mongodb");
+const dotenv = require("dotenv");
+dotenv.config();
 
-// describe("Test Handlers", () => {
-//   test("responds to /", async () => {
-//     const res = await request.get("/snack");
-//     console.log(res);
-//     expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
-//     expect(res.statusCode).toBe(200);
-//   });
+describe("Test Handlers", () => {
+  let connection;
+  let request;
+  let createSnackId;
 
-//   test("responds to /users", async () => {
-//     const res = await request.get("/snack");
-//     expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
-//     expect(res.statusCode).toBe(200);
-//   });
-// });
+  beforeAll(async () => {
+    connection = await MongoClient.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    request = supertest(app);
+  });
+  afterAll(async () => {
+    await connection.close();
+  });
+  test("Get all /snack", async () => {
+    const res = await request.get("/snack");
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.statusCode).toBe(200);
+  });
+
+  test("Get /snack/id", async () => {
+    const res = await request.get("/snack/64a85fad71d32c1c168b17e7");
+    console.log(res);
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.statusCode).toBe(200);
+  });
+
+  test("Create new /snack", async () => {
+    const res = await request.post("/snack").send({
+      type: "none",
+      maker: "Cocacola",
+      name: "Doritos",
+      ounces: "8oz",
+      calories: "2000",
+      sugar: "30gr",
+      totalFat: "30",
+      ingredients: "sugar, salt",
+    });
+    createSnackId = res.body._id;
+    console.log(res);
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.statusCode).toBe(201);
+  });
+
+  // test("Create new /snack", async () => {
+  //   const res = await request.put(`/snack/${createSnackId}`).send({
+  //     type: "none",
+  //     maker: "Cocacola",
+  //     name: "Doritos",
+  //     ounces: "8oz",
+  //     calories: "2000",
+  //     sugar: "30gr",
+  //     totalFat: "30",
+  //     ingredients: "sugar, salt",
+  //   });
+    // expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    // expect(res.statusCode).toBe(204);
+  // });
+  // test("Delete /snack/id", async () => {
+  //   const res = await request.delete("/snack/64a85fad71d32c1c168b17e7");
+  //   expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+  //   expect(res.statusCode).toBe(204);
+  // });
+
+});
